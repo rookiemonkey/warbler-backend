36302428a78eb09f22828bf95b974585765cd7a1
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-17 20:47:45 +0800

    added nodemon

--- models/index.js
+++ models/index.js
@@ -13,8 +13,8 @@ mongoose.connect(
 )
 
 // check mongoose connection
-mongoose.connection.on("open", ()=>{console.log(`CONNECTED TO MONGODATA BASE TIMESTAMP: ${Date()}`)})
-mongoose.connection.on("error", ()=>{console.error(`FAILED TO CONNECT TO THE DATABASE...`)})
+mongoose.connection.on("open", () => { console.log(`CONNECTED TO MONGODATA BASE TIMESTAMP: ${Date()}`) })
+mongoose.connection.on("error", () => { console.error(`FAILED TO CONNECT TO THE DATABASE...`) })
 
 // directory being imported on /handlers/auth.js
 // bundles everything inside this directory
