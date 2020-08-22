6d4aaac40119ee961659a13da4ed8f54b43cac13
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-22 13:35:48 +0800

    realigned message routes

--- routes/messages-all.js
+++ routes/messages-all.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true })
 const getMsgAll = require('../handlers/message_readAll')
 
+// prefix - /api/message/all"
 router.route("/")
     .get(getMsgAll)
 
--- routes/messages.js
+++ routes/messages.js
@@ -4,11 +4,11 @@ const getMsg = require('../handlers/message_read')
 const createMsg = require('../handlers/message_create')
 const deleteMsg = require('../handlers/message_delete')
 
-// prefix - /api/users/:id/message
-router.route("/")
+// prefix - /api/message/:id
+router.route("/new")
     .post(createMsg)
 
-// prefix - api/users/:id/message/:message_id
+// prefix - /api/message/:id"
 router.route("/:message_id")
     .get(getMsg)
     .delete(deleteMsg)
