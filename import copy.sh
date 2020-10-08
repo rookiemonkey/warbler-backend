3725b9fbb48c3e07a19e9874309929bc38a43762
rookiemonkey <kevinroirigorbasina@pm.me>
2020-10-09 07:13:44 +0800

    for deployment

--- import copy.sh
+++ import copy.sh
@@ -1,10 +1,7 @@
-heroku config:set DB_URL="mongodb+srv://kevin12345:kevin12345@warbler-nvqky.mongodb.net/warbler?retryWrites=true&w=majority" &&
-
-heroku config:set SECRET_KEY_JWT="as894813eqrwf65a1sd897avsd3982dkfj928fashifuhoaiuehs98e" &&
-heroku config:set SECRET_KEY_OTP="1A2SDF78A738;[D937DuoD886595asdf]ae23f2(as7f8davsd3)" &&
-
-heroku config:set CLOUDINARY_API_KEY="787316785326125" &&
-heroku config:set CLOUDINARY_API_SECRET="8UvFT6Qn6KsjJgnztyxwkU6jxis" &&
-
-heroku config:set GMAILUN="kevinroirigorbasina@gmail.com" &&
-heroku config:set GMAILPW="pvffzjhkvluadcyd"
+heroku config:set DB_URL="" &&
+heroku config:set SECRET_KEY_JWT="" &&
+heroku config:set SECRET_KEY_OTP="" &&
+heroku config:set CLOUDINARY_API_KEY="" &&
+heroku config:set CLOUDINARY_API_SECRET="" &&
+heroku config:set GMAILUN="" &&
+heroku config:set GMAILPW=""
