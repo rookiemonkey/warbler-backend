c118c794e61c9bfaed73eb06b846294b04bff3cf
rookiemonkey <kevinroirigorbasina@pm.me>
2020-10-09 13:47:41 +0800

    added a route to fetch all messages of a specific user only

---
+++ handlers/message_readAllOfUser.js
@@ -0,0 +1,26 @@
+const Message = require('../models/message')
+
+const getMsgAllOfUser = async function (req, res, next) {
+    try {
+
+        // find all, sort descending, then populate the user key its username/picture
+        const messages = await Message
+            .find({ _id: req.params.userid })
+            .sort({ updatedAt: "desc" })
+            .populate("user", `username profilePicture`)
+
+        // return as json
+        res
+            .status(200)
+            .json(messages)
+    }
+
+    catch (err) {
+
+        return next(err)
+
+    }
+
+}
+
+module.exports = getMsgAllOfUser;
--- routes/messages-all.js
+++ routes/messages-all.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true })
 const getMsgAll = require('../handlers/message_readAll')
+const getMsgAllOfUser = require('../handlers/message_readAllOfUser');
 
 // prefix - /api/message/all"
 router.route("/")
     .get(getMsgAll)
+
+router.route("/:userid")
+    .get(getMsgAllOfUser)
 
 module.exports = router
